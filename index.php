fa767c55173b4844cf436f4ba5f4e23ca6a648e6
oswaldo olea <o.olea@sintraconsulting.com>
2022-09-26 07:41:19 +0200

    new menu changes

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+
+<style>
+h2 {
+  color: grey;
+  margin-left: 20px;
+}
+</style>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+
+
+
+
+<body>
+
+<h1 style='font-family: "Arial";'>site construction in progress</h1>
+
+</body>
+</html>
